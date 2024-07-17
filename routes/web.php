2ae5921b6ea895140126827847dea0a45ee0b225
Tudorinho <dantudor_02@yahoo.com>
2024-07-18 01:11:33 +0300

    homepage is now the default page

--- routes/web.php
+++ routes/web.php
@@ -16,25 +16,6 @@ Route::get('/vopsele-lavabile-2', function () {
     return view('categories.vopsele-lavabile2');
 });
 
-Route::get('/homepage', function () {
-    return view('homepage');
-});
-
-
-if (!function_exists('include_all_css')) {
-    function include_all_css($directory)
-    {
-        $output = '';
-        $cssFiles = glob(public_path($directory) . '/*.css');
-        foreach ($cssFiles as $file) {
-            $relativePath = str_replace(public_path(), '', $file);
-            $output .= '<link rel="stylesheet" href="' . asset($relativePath) . '">' . "\n";
-        }
-        return $output;
-    }
-}
-
 Route::get('/', function () {
-    $cssLinks = include_all_css('css');
-    return view('main-body', ['cssLinks' => $cssLinks]);
+    return view('homepage');
 });
