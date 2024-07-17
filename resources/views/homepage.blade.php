1389161ffeea93a0d9b29098d0e0c72931f5ba63
Tudorinho <dantudor_02@yahoo.com>
2024-07-17 15:13:29 +0300

    custom css for layout

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,32 +1,38 @@
 @extends('layout.layout')
 
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/homepage.css') }}">
+@endsection
+
+
 @section('content')
 
-<div class="w-full" id="homepage">
-    @include('site.partials.homepage-slider')
-    {{-- <div id="homepage-header" style="background-image: url('{{ asset('images/images/Lacuri-si-vopsele-emex.jpg') }}')">
-        <div class="main-container grid-3">
-            
-            <div class="header_img_bg col justify-center align-center" id="mod_avl">
-                <h1 class="z-10">
-                    Vopsele, Tencuieli, Pardoseli<br>EMEX By Romtehnochim
-                </h1>
+    <div class="w-full" id="homepage">
+        @include('site.partials.homepage-slider')
+        {{-- <div id="homepage-header" style="background-image: url('{{ asset('images/images/Lacuri-si-vopsele-emex.jpg') }}')">
+            <div class="main-container grid-3">
+                
+                <div class="header_img_bg col justify-center align-center" id="mod_avl">
+                    <h1 class="z-10">
+                        Vopsele, Tencuieli, Pardoseli<br>EMEX By Romtehnochim
+                    </h1>
+                </div>
             </div>
+        </div> --}}
+
+
+
+        @include('site.partials.homepage-about')
+
+
+        <div class="bg-blue" id="hp_first_paralax">
+        <div class="container hp_paralax_text">
+            &#8220;Emex&#8221; - Performanta si profesionalism !
         </div>
-    </div> --}}
-
-
-
-    @include('site.partials.homepage-about')
-
-
-    <div class="bg-blue" id="hp_first_paralax">
-    <div class="container hp_paralax_text">
-        &#8220;Emex&#8221; - Performanta si profesionalism !
+        </div>
+        @include('site.partials.homepage-cards')
     </div>
-    </div>
-    @include('site.partials.homepage-cards')
-</div>
 
 
 @endsection
--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -6,41 +6,16 @@
     <title>Main Page</title>
     <!-- Include fișierele CSS din directorul specificat -->
     <link rel="stylesheet" href="{{ asset('css/adrian.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/angajari.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/aplicare.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/blog.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/cart.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/cartela-lavabile-culori.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/cartela-ral-culori.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/certificari.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/consum.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/despre.css') }}">
     <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
     <link rel="stylesheet" href="{{ asset('css/form.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/gallery.css') }}">
     <link rel="stylesheet" href="{{ asset('css/generic.css') }}">
     <link rel="stylesheet" href="{{ asset('css/header.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/homepage.css') }}">
     <link rel="stylesheet" href="{{ asset('css/layout.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/lightbox.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/my-account.css') }}">
     <link rel="stylesheet" href="{{ asset('css/new.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/order.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/pagination.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/plata.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/politica-de-calitate.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/politica-de-mediu.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/politica-de-securitate.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/product-card.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/product-page.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/produs.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/retur.css') }}">
     <link rel="stylesheet" href="{{ asset('css/root.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/search.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/servicii.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/solicita-cotatie.css') }}">
     <link rel="stylesheet" href="{{ asset('css/sprite.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/tabs.css') }}">
+    @yield('css')
+    {{-- @vite('resources/css/app.css') --}}
 </head>
 
 
@@ -207,7 +182,7 @@
         <img width="50" height="50" src="{{ asset('images/images/Mail-mobile.png') }}" alt="Email Emex">
     </div>
 
-    <div>
+    <div class="m-0">
         @yield('content')
     </div>
 
