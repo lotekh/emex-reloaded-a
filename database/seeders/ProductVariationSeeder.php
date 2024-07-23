2703f923cdb06386db6816c6e80ace4253079b74
Irina Vaghin <irina.vaghin@webitfactory.io>
2024-07-23 17:21:32 +0300

    Added product variation seeder

---
+++ database/seeders/ProductVariationSeeder.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\MeasurementUnit;
+use App\Models\Product;
+use App\Models\ProductVariation;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+
+class ProductVariationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Schema::disableForeignKeyConstraints();
+        ProductVariation::truncate();
+        Schema::enableForeignKeyConstraints();
+
+        $csvFile = resource_path('csv/preturi_culori.csv');
+        $fileHandle = fopen($csvFile, 'r');
+
+        $rowKey = 0;
+        while ($csvRow = fgetcsv($fileHandle, null, ',')) {
+            if($rowKey) {
+                $product = Product::where('slug', $csvRow[0])->first();
+                $quantityAndMeasurementUnit = explode(' ', $csvRow[1]);
+                $measurementUnit = MeasurementUnit::where('name', $quantityAndMeasurementUnit[1])->first();
+
+                if($product && $measurementUnit) {
+                    ProductVariation::create([
+                        'product_id' => $product->id,
+                        'measurement_unit_id' => $measurementUnit->id,
+                        'quantity' => $quantityAndMeasurementUnit[0],
+                        'colour' => $csvRow[2],
+                        'price' => $csvRow[5],
+                        'name' => $csvRow[6],
+                        'addon_text' => $csvRow[7],
+                        'ean' => $csvRow[8],
+                        'sku' => $csvRow[9],
+                        'weight' => $csvRow[10],
+                    ]);
+                }
+                else {
+                    print_r('Product or measurement unit not found for row ' . $rowKey . PHP_EOL);
+                }
+                
+            }
+
+            $rowKey++;
+        }
+        fclose($fileHandle);
+    }
+}
