0f6075286fe3e98113cf6b05cc27036c6775a44c
Irina Vaghin <irina.vaghin@webitfactory.io>
2024-07-25 13:04:30 +0300

    Fixed database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,8 +13,13 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            UserSeeder::class,
             SettingsSeeder::class,
             CountryAndCountySeeder::class,
+            MeasurementUnitSeeder::class,
+            CategorySeeder::class,
+            ProductSeeder::class,
+            ProductVariationSeeder::class
         ]);
     }
 }
