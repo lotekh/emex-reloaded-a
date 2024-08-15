9418d825b655fceac3ea6cdb2a158e990e609ce5
DanTudor10 <dan.tudor@webitfactory.io>
2024-08-15 12:20:03 +0300

    feat: vopsire-epoxidica-pardoseli blade

--- resources/views/servicii/vopsire-epoxidica-pardoseli.blade.php
+++ resources/views/servicii/vopsire-epoxidica-pardoseli.blade.php
@@ -5,69 +5,262 @@ $baseUrl = url('/');
 ?>
 
 @section('header_title')
-    
+Vopsea Epoxidica pentru Pardoseala<br>din Beton
 @endsection
 
 @section('section_1_title')
-    
+VOPSEA EPOXIDICA - PARDOSELI BETON
 @endsection
 
 @section('section_1_text')
-    
+Sistemele de <strong><em>vopsele epoxidice de pardoseala</em></strong>, dezvoltate de “Romtehnochim”, in gama de produse specializate “Emex”,
+sunt produse de ultima generatie, de inalta calitate, ce sunt adaptate, inca din procesul de productie, atat la tipul de suprafata pe care
+sunt aplicate, cat si la destinatia finala a acoperirii. Acestea vin sa ofere un raspuns superior calitativ, atat din punct de vedere
+structural, cat si estetic atunci cand sunt aplicate cu profesionalism, de personal calificat.
 @endsection
 
 @section('link_vezi_produsul')
-    
+/vopsea-epoxidica-pardoseli
 @endsection
 
 @section('link_fisa_tehnica')
-    
+/doc/vopsea-epoxidica-pardoseli.pdf
 @endsection
 
 @section('section_2_title')
-    
+Proprietati: Vopsea Pardoseala din Beton “Emex”
 @endsection
 
 @section('section_2_text')
-    
+Multitudinea de aplicatii pentru pardoseli, la care <strong><em>Vopseaua Epoxidica
+    pentru Beton “Emex”</em></strong> se poate folosi, se datoreaza in special urmatoarelor
+proprietati:
 @endsection
 
 @section('section_2_list')
-    
+<li>Duritate mare;</li>
+<li>Etalare excelenta;</li>
+<li>Grad de luciu superior;</li>
+<li>Rezistenta mare la abraziune;</li>
+<li>Rezistenta in medii corozive;</li>
+<li>Rezistenta mare la hidrocarburi;</li>
+<li>Rezistenta mare la trafic si uzura;</li>
+<li>Rezistenta mare la socuri mecanice;</li>
+<li>Suporta excelent ciclurile inghet-dezghet.</li>
 @endsection
 
 @section('section_3_title')
-    
+Vopsele epoxidice de pardoseala
 @endsection
 
 @section('section_3_text')
-    
+Exista diverse tipuri de sisteme de <strong>vopsea epoxidica de pardoseala</strong>, care au scopuri estetice si functionale diferite, din
+care putem evidentia efectele de culoare, efectul antiderapant, aplicarea de fulgi colorati la supralacuire, care sunt cel mai des intalnite.
+Pentru a obtine rezultate superioare, cu orice vopsea pentru beton, alaturi de calitatea vopselei, este necesara si o buna experienta a
+aplicatorilor, elemente de care depinde realizarea unei vopsiri de efect.
 @endsection
 
 @section('section_3_list')
-    
+<li>
+    Produsele din aceasta categorie sunt bicomponente, alcatuite din baza vopselei, realizate din <strong><em>rasini epoxidice</em></strong>,
+    pigmenti de colorare, extenderi si aditivi, cea de-a doua componenta fiind intaritorul de tip poliaminic.
+</li>
+<li>
+    Diversitatea culorilor si a metodelor de aplicare determina o plaja foarte mare de aplicatii, de mare efect estetic si cu o rezistenta
+    mult superioara altor vopsele sau sisteme conventionale.
+</li>
 @endsection
 
 @section('video')
-    
+<video controls width="250" height="168" class="responsive" poster="https://vopsele.xyz/videos/Vopsire-epoxidica.jpg">
+    <source src="https://vopsele.xyz/videos/vopsire-epoxidica.mp4" type="video/mp4">
+        Vopsire cu sisteme epoxidice pentru pardoseli industriale, hale, zone cu circulatie intensa, hypermarket-uri, etc. Etapele aplicarii vopselei epoxidice sunt identice si in cazul aplicarii vopselei poliuretanice.
+</video>
 @endsection
 
 @section('section_4_title')
-    
+Pasi in aplicare: Vopsea Epoxidica Beton
 @endsection
 
 @section('section_4_text')
-   
+<span class="alineat_span"></span>In unele cazuri, cu scopul de a mari protectia suprafetei, si/sau gradul de luciu, peste stratul principal, se aplica un lac pe baza
+de <strong><em>rasina epoxidica</em></strong>, indiferent de tipul acestuia, in cazul in care vopsirea pentru beton s-a facut la interior.
+Daca vopsirea s-a facut la exterior, se va aplica un
+<a href="<?= $baseUrl ?>/lac-poliuretanic-protectie-radiatii" title="Lac de supralacuire si protectie UV">
+    <strong><em>Lac Poliuretanic de Protectie “Emex UV Shield”</em></strong>
+</a>, un top-coat rezistent la lumina si radiatii UV, care asigura suprafetei un excedent de rezistenta si luciu,
+marind perioada de utilizare a suprafetei protejate, asigurandu-i in plus si o estetica excelenta.
+</p>
 @endsection
 
 @section('section_4_list')
-    
+<li>
+    Se inlatura toate urmele de murdarie, stropiri accidentale, restul de materiale adezive, etc.
+</li>
+<li>
+    Se sableaza sau se asperizeaza suportul cu masini cu bile, discuri diamantate, sau scarificator pentru beton.
+</li>
+<li>
+    Se degreseaza suportul pana la eliminarea completa a tuturor contaminantilor, care ar putea influenta
+    calitatea finala a vopsirii epoxidice pentru pardoseli din beton.
+</li>
+<li>
+    Se creaza o maxima compatibilitate intre stratul suport si vopseaua epoxidica, prin impregnarea suportului cu
+    <a href="<?= $baseUrl ?>/amorsa-impregnare-pardoseli-epoxidice" title="Amorsa epoxidica pentru vopsea beton">
+        <strong><em>Amorsa Epoxidica de Impregnare “Emex”</em></strong>
+    </a>, ce are si rol de fixare a suportului.
+</li>
+<li>
+    Urmeaza vopsirea finala care se va face in doua straturi, prin roluire, sau pulverizare cu instalatii air-less,
+    daca este posibil si amplasamentul o permite.
+</li>
+<li>
+    Se va aplica un top-coat ca strat final, daca se doreste fie un luciu deosebit, fie o mai buna protectie la lumina
+    cu produse cum ar fi:
+    <a href="<?= $baseUrl ?>/lac-epoxidic-solventat" title="Lac epoxidic solventat pentru supralacuire">
+        <strong><em>Lac Epoxidic de Protectie “Emex Epoxy Coat”</em></strong>
+    </a>, sau cu
+    <a href="<?= $baseUrl ?>/lac-epoxidic-sigilare" title="Lac epoxidic nesolventat pentru sigilare">
+        <strong><em>Lac Epoxidic de Sigilare “Emex Epoxy Contact”</em></strong>
+    </a>,
+    acesta din urma asigurand o si mai mare duritate si rezistenta la trafic sau zgariere. La exterior, se va folosi in mod obligatoriu
+    <a href="<?= $baseUrl ?>/lac-poliuretanic-protectie-radiatii" title="Lac de supralacuire si protectie UV">
+        <strong><em>Lac Poliuretanic de Protectie “Emex UV Shield”</em></strong>
+    </a>, care are rolul de a asigura protectia la lumina si radiatii UV.
+</li>
 @endsection
 
 @section('section_5_title')
-    
+Vopsele epoxidice aplicate de Romtehnochim
 @endsection
 
 @section('section_5_images')
-    
+<div>
+    <x-lightbox 
+        id="prod-lightbox1"
+        class="cover cursor-pointer"
+        url="{{ asset('images/landing/mici/Epoxidica-birouri.jpg') }}"
+        lightboxSrc="{{ asset('images/landing/mari/Vopsire-epoxidica-birou.jpg') }}"
+        alt="Aplicare rasina epoxidica in zona birouri"
+        title="Aplicare rasina epoxidica in zona birouri"
+        width="268"
+        height="173"
+        layout="responsive"
+        lightboxWidth="600"
+        lightboxHeight="450"
+    />
+</div>
+
+<div>
+    <x-lightbox 
+        id="prod-lightbox2"
+        class="cover cursor-pointer"
+        url="{{ asset('images/landing/mici/Service-auto-vopsire-epoxidica.jpg') }}"
+        lightboxSrc="{{ asset('images/landing/mari/Aplicare-epoxidica-service.jpg') }}"
+        alt="Aplicare vopsea epoxidica in statie service"
+        title="Aplicare vopsea epoxidica in statie service"
+        width="268"
+        height="173"
+        layout="responsive"
+        lightboxWidth="600"
+        lightboxHeight="450"
+    />
+</div>
+
+<div>
+    <x-lightbox 
+        id="prod-lightbox3"
+        class="cover cursor-pointer"
+        url="{{ asset('images/landing/mici/Vopsire-hala-industriala.jpg') }}"
+        lightboxSrc="{{ asset('images/landing/mari/Hala-industriala.jpg') }}"
+        alt="Vopsea pentru pardoseli beton executie Romtehnochim"
+        title="Vopsea pentru pardoseli beton executie Romtehnochim"
+        width="268"
+        height="173"
+        layout="responsive"
+        lightboxWidth="600"
+        lightboxHeight="450"
+    />
+</div>
+
+<div>
+    <x-lightbox 
+        id="prod-lightbox4"
+        class="cover cursor-pointer"
+        url="{{ asset('images/landing/mici/Epoxidica-spatiu-productie.jpg') }}"
+        lightboxSrc="{{ asset('images/landing/mari/Vopsire-spatiu-productie.jpg') }}"
+        alt="Vopsea de beton pentru spatiu productie"
+        title="Vopsea de beton pentru spatiu productie"
+        width="268"
+        height="173"
+        layout="responsive"
+        lightboxWidth="600"
+        lightboxHeight="450"
+    />
+</div>
+
+<div>
+    <x-lightbox 
+        id="prod-lightbox5"
+        class="cover cursor-pointer"
+        url="{{ asset('images/landing/mici/Service-Dacia-vopsire-epoxidica.jpg') }}"
+        lightboxSrc="{{ asset('images/landing/mari/Vopsire-epoxidica-service.jpg') }}"
+        alt="Aplicare vopsea rasini epoxidice service auto"
+        title="Aplicare vopsea rasini epoxidice service auto"
+        width="268"
+        height="173"
+        layout="responsive"
+        lightboxWidth="600"
+        lightboxHeight="450"
+    />
+</div>
+
+<div>
+    <x-lightbox 
+        id="prod-lightbox8"
+        class="cover cursor-pointer"
+        url="{{ asset('images/landing/mici/Aplicare-epoxidica-subsol.jpg') }}"
+        lightboxSrc="{{ asset('images/landing/mari/Vopsire-epoxidica-statie-pompare.jpg') }}"
+        alt="Vopsire subsol cu vopsea beton epoxidica"
+        title="Vopsire subsol cu vopsea beton epoxidica"
+        width="268"
+        height="173"
+        layout="responsive"
+        lightboxWidth="600"
+        lightboxHeight="450"
+    />
+</div>
+
+<div>
+    <x-lightbox 
+        id="prod-lightbox9"
+        class="cover cursor-pointer"
+        url="{{ asset('images/landing/mici/Vopsea-epoxidica-garaj.jpg') }}"
+        lightboxSrc="{{ asset('images/landing/mari/Epoxidica-pardoseala-garaj.jpg') }}"
+        alt="Vopsire de pardoseala epoxidica garaj"
+        title="Vopsire de pardoseala epoxidica garaj"
+        width="268"
+        height="173"
+        layout="responsive"
+        lightboxWidth="600"
+        lightboxHeight="450"
+    />
+</div>
+
+<div>
+    <x-lightbox 
+        id="prod-lightbox10"
+        class="cover cursor-pointer"
+        url="{{ asset('images/landing/mici/Linie-de-productie-fabrica.jpg') }}"
+        lightboxSrc="{{ asset('images/landing/mari/Pardoseala-vopsita-epoxy-fabrica.jpg') }}"
+        alt="Aplicare industriala vopsea epoxidica pardoseala"
+        title="Aplicare industriala vopsea epoxidica pardoseala"
+        width="268"
+        height="173"
+        layout="responsive"
+        lightboxWidth="600"
+        lightboxHeight="450"
+    />
+</div>
+
 @endsection
