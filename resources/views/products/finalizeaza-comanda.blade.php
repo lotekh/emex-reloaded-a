9d538f26ed5caa866fb9da7eee016fd4999a21d9
Tudorinho <dantudor_02@yahoo.com>
2024-09-13 12:44:58 +0300

    feat: breacrumbs on finalizeza-comanda and angajari

--- resources/views/products/finalizeaza-comanda.blade.php
+++ resources/views/products/finalizeaza-comanda.blade.php
@@ -4,6 +4,10 @@
     <link rel="stylesheet" href="{{ asset('css/order.css') }}">
 @endsection
 
+@section('breadcrumbs')
+<ul class="flex gap-xs"><li class="font-xs"><a href="/">Acasa</a></li><li class="separator">/</li><li class="font-xs -ml-4"><a href="/produse">Produse</a></li><li class="separator">/</li><li class="font-xs -ml-4 ellipsis"><a href="#">Produse adaugate</a></li></ul>
+@endsection
+
 @section('content')
 <div class="main-container" id="order-page">
     <form method="POST" action="{{ url('/save-order') }}" id="order_form">
