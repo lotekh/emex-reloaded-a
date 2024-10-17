fcdf7ba3952233e6e3ede058f1d73cfc4a275930
Irina Vaghin <irina.vaghin@webitfactory.io>
2024-10-17 17:01:51 +0300

    Fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Schema::defaultStringLength(255);
+        Schema::defaultStringLength(191);
 
         // Share categories with all views
         if(Schema::hasTable('categories')) {
