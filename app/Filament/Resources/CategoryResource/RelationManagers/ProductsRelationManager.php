53972b44d41a5cd4f1fb704c570e54ccb3fa7b3b
Irina Vaghin <irina.vaghin@webitfactory.io>
2024-11-15 15:24:38 +0200

    Reordering products in category feature

---
+++ app/Filament/Resources/CategoryResource/RelationManagers/ProductsRelationManager.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Filament\Resources\CategoryResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class ProductsRelationManager extends RelationManager
+{
+    protected static string $relationship = 'products';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('plain_name')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('plain_name')
+            ->columns([
+                Tables\Columns\TextColumn::make('plain_name'),
+                Tables\Columns\TextColumn::make('order'),
+            ])
+            ->defaultSort('order')
+            ->defaultPaginationPageOption(25)
+            ->reorderable('order');
+    }
+}
