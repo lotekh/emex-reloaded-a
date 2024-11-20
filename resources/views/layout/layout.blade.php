02801c8d727ca2055f295a6adcde26440f63075f
Tudorinho <dantudor_02@yahoo.com>
2024-11-20 05:18:38 +0200

    fixes from the archive and product page

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -3,10 +3,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Main Page</title>
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    @yield('title')
     <link rel="icon" type="image/x-icon" href="{{ asset('resources/emex-favicon.ico') }}">
 
 
