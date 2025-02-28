e248b1918b386762d9e426302bac10d307a76327
Irina Vaghin <irina.vaghin@webitfactory.io>
2025-02-28 17:07:56 +0200

    Fix on image path

--- app/Filament/Resources/OrganizedMediaResource/Pages/CreateOrganizedMedia.php
+++ app/Filament/Resources/OrganizedMediaResource/Pages/CreateOrganizedMedia.php
@@ -9,4 +9,11 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateOrganizedMedia extends CreateRecord
 {
     protected static string $resource = OrganizedMediaResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['path'] = 'media/' . $data['type'] . '/' . $data['name'];
+
+        return $data;
+    }
 }
