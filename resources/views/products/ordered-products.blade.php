debf2cdb4ae89fcf4be1ab524cc38967460388b7
Tudorinho <dantudor_02@yahoo.com>
2025-03-12 18:50:00 +0200

    chore: update comments on ordered-products

--- resources/views/products/ordered-products.blade.php
+++ resources/views/products/ordered-products.blade.php
@@ -173,7 +173,7 @@
               <div class="flex align-center mt-16">
                 <span class="bold mr-8">Cantitate: </span>
                 <div class="quantity-selector quantity-selector-mobile flex">
-                  {{-- Scade cantitatea --}}
+                  {{-- Lower the quantity --}}
                   <form method="POST" action="{{ route('orders.updateQuantity') }}">
                     @csrfWithoutAutocomplete
                     <input type="hidden" name="product_variation_id" value="{{ $ordered_product->id }}">
@@ -181,7 +181,7 @@
                     <button type="submit" aria-label="Scade cantitatea">-</button>
                   </form>
                   {{ $ordered_product->ordered_quantity }}
-                  {{-- Creste cantitatea --}}
+                  {{-- Up the quantity --}}
                   <form method="POST" action="{{ route('orders.updateQuantity') }}">
                     @csrfWithoutAutocomplete
                     <input type="hidden" name="product_variation_id" value="{{ $ordered_product->id }}">
@@ -203,7 +203,7 @@
             </div>
     
             <div class="p-16 pr-8 flex align-end gap-md">
-              {{-- Buton de ștergere --}}
+              {{-- Remove Product button --}}
               <form method="POST" action="{{ route('orders.removeProduct') }}">
                 @csrfWithoutAutocomplete
                 <input type="hidden" name="product_variation_id" value="{{ $ordered_product->id }}">
@@ -212,7 +212,7 @@
                 </button>
               </form>
             
-              {{-- Buton de adăugare la favorite --}}
+              {{-- Wishlist button --}}
               <form method="POST" class="addToWishlistBt" action="{{ route('wishlist.toggle') }}">
                 @csrfWithoutAutocomplete
                 <input type="hidden" name="product_id" value="{{ $ordered_product->product->id }}">
@@ -223,14 +223,13 @@
                 </button>
               </form>
             
-              {{-- Iconiță de editare (creion) --}}
+              {{-- Mentions button --}}
               <button class="edit-btn" aria-label="Editează produsul">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
                   <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
                 </svg>
               </button>
-            </div>
-            
+            </div>       
 
           </div>
         </div>
