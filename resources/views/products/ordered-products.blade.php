b9f750750f970b11234f176dbd570ede0a4827ab
Tudorinho <dantudor_02@yahoo.com>
2025-03-12 18:48:34 +0200

    fix styling for ordered-products on mobile

--- resources/views/products/ordered-products.blade.php
+++ resources/views/products/ordered-products.blade.php
@@ -104,12 +104,14 @@
                   <td class="price">
                       <div class="flex justify-between">
                           <p class="price">{{ number_format($totalIndividualPrice, 2) }} Lei</p>
-                          <form method="POST" action="{{ route('orders.removeProduct') }}">
-                              @csrfWithoutAutocomplete
-                              <input type="hidden" name="product_variation_id" value="{{ $ordered_product->id }}">
-                              <button aria-label="Sterge produsul"><img src="{{ asset('resources/new_design/icons/bin.svg') }}" width="18" height="18"></button>
-                          </form>
                       </div>
+                  <td>
+                    <form method="POST" action="{{ route('orders.removeProduct') }}">
+                      @csrfWithoutAutocomplete
+                      <input type="hidden" name="product_variation_id" value="{{ $ordered_product->id }}">
+                      <button aria-label="Sterge produsul"><img src="{{ asset('resources/new_design/icons/bin.svg') }}" width="18" height="18"></button>
+                    </form>
+                  </td>
                   <td>
                       @php
                         $productId = $ordered_product->product->id;
@@ -200,7 +202,8 @@
               <p class="mb-8 mt-8"><span class="bold">Cost: </span> &nbsp; {{ number_format($totalIndividualPrice, 2) }} Lei (TVA inclus)</p>
             </div>
     
-            <div class="p-16 flex align-end">
+            <div class="p-16 pr-8 flex align-end gap-md">
+              {{-- Buton de ștergere --}}
               <form method="POST" action="{{ route('orders.removeProduct') }}">
                 @csrfWithoutAutocomplete
                 <input type="hidden" name="product_variation_id" value="{{ $ordered_product->id }}">
@@ -208,7 +211,27 @@
                   <img src="{{ asset('resources/new_design/icons/bin.svg') }}" width="18" height="18" alt="Sterge produsul">
                 </button>
               </form>
+            
+              {{-- Buton de adăugare la favorite --}}
+              <form method="POST" class="addToWishlistBt" action="{{ route('wishlist.toggle') }}">
+                @csrfWithoutAutocomplete
+                <input type="hidden" name="product_id" value="{{ $ordered_product->product->id }}">
+                <input type="hidden" name="product_variation_id" value="{{ $ordered_product->id }}">
+                <input type="hidden" name="remove_from_cart" value="1">
+                <button class="wishlist-btn-cos" aria-label="{{ $isInWishlist ? 'Elimină din favorite' : 'Adaugă la favorite' }}">
+                  <img width="20" height="20" src="{{ $isInWishlist ? asset('resources/new_design/icons/star-fill.svg') : asset('resources/new_design/icons/star.svg') }}" title="Muta la Favorite" alt="wishlist">
+                </button>
+              </form>
+            
+              {{-- Iconiță de editare (creion) --}}
+              <button class="edit-btn" aria-label="Editează produsul">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+                  <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
+                </svg>
+              </button>
             </div>
+            
+
           </div>
         </div>
       @endforeach
