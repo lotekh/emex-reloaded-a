fc68db0edee26a48132646e9f43cd80167a9f936
Tudorinho <dantudor_02@yahoo.com>
2025-03-18 12:48:17 +0200

    chore: first draft for mentions for mobile

--- resources/views/products/ordered-products.blade.php
+++ resources/views/products/ordered-products.blade.php
@@ -228,7 +228,7 @@
               </form>
             
               {{-- Mentions button --}}
-              <button class="edit-btn mb-4" aria-label="Editează produsul">
+              <button class="edit-btn mb-4 mention-btn" aria-label="Editează produsul" data-product-id="{{ $ordered_product->id }}" data-current-mention="{{ $ordered_product->mentions ?? '' }}">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
                   <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
                 </svg>
@@ -269,7 +269,7 @@
 </div>
 
 <div id="mentionModal" class="lightbox-container hidden-important">
-  <div class="bg-white p-8 shadow-lg w-96">
+  <div class="bg-white p-8" id="mention-modal-box">
       <h2 class="text-lg font-bold mb-4">Mențiune pentru produs</h2>
       <textarea id="mentionText" class="w-full p-2 border rounded" rows="3"></textarea>
       <input type="hidden" id="mentionProductId">
