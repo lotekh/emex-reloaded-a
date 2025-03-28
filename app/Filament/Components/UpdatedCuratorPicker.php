97192e632a559cc1fa782bc93d79d431a773f226
Irina Vaghin <irina.vaghin@webitfactory.io>
2025-03-28 11:20:52 +0200

    Improved curator picker

--- app/Filament/Components/UpdatedCuratorPicker.php
+++ app/Filament/Components/UpdatedCuratorPicker.php
@@ -5,6 +5,7 @@ namespace App\Filament\Components;
 use App\Filament\Resources\OrganizedMediaResource;
 use Awcodes\Curator\Components\Forms\CuratorPicker;
 use Awcodes\Curator\PathGenerators\DefaultPathGenerator;
+use Filament\Facades\Filament;
 use Filament\Forms\Components\Actions\Action;
 use Illuminate\Support\Facades\App;
 
@@ -20,6 +21,20 @@ class UpdatedCuratorPicker extends CuratorPicker {
                 return App::make(OrganizedMediaResource::class)
                     ->getUrl('edit', ['record' => $arguments['id']]);
             }, true);
+    }
+
+    public function getDirectory(): string
+    {
+        $className = get_class($this->getRelationship()->getParent());
+        $relationshipName = $this->getRelationshipName();
+
+        if(str_contains($className, 'BlogArticle')) {
+            return config('curator.directory') . '/blog';
+        }
+        else if($relationshipName == 'technicalFile') {
+            return config('curator.directory') . '/technical-files';
+        }
+        return config('curator.directory') . '/images';
     }
 }
 
