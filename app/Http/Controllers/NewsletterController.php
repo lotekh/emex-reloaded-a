b882e6921bb6f53efe371a4f53c18a26b126228e
DanTudor10 <dan.tudor@webitfactory.io>
2025-04-22 16:25:53 +0300

    feat: add email to user when subscribing to Newsletter

--- app/Http/Controllers/NewsletterController.php
+++ app/Http/Controllers/NewsletterController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\NewsletterEmail;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Log;
 
 class NewsletterController extends Controller
 {
@@ -22,6 +24,32 @@ class NewsletterController extends Controller
         $newsletterEmail->page_url = $request->input('current_url');
         $newsletterEmail->ip = $request->ip();
         $newsletterEmail->save();
+
+        try {
+            $fromEmail = 'info@emex.ro'; 
+            $clientEmail = $newsletterEmail->email;
+    
+            $emailContent = "Salut,\n\n";
+            $emailContent .= "Îți mulțumim că te-ai abonat la newsletterul nostru!\n";
+            $emailContent .= "Toate cele bune,\n";
+            $emailContent .= "Echipa Romtehnochim";
+    
+            Mail::raw($emailContent, function ($message) use ($clientEmail, $fromEmail) {
+                $message->to($clientEmail)
+                    ->from($fromEmail, 'Romtehnochim')
+                    ->subject('Confirmare abonare la newsletter');
+            });
+    
+            Log::info('Email de confirmare newsletter trimis:', [
+                'email' => $clientEmail,
+            ]);
+    
+        } catch (\Exception $e) {
+            Log::error('Eroare la trimiterea emailului de confirmare newsletter:', [
+                'message' => $e->getMessage(),
+                'trace' => $e->getTraceAsString(),
+            ]);
+        }
 
         return redirect()->back()->with('success', 'Te-ai abonat cu succes la newsletter!');
     }
