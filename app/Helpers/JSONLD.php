adf1ad3974450ec4de2ae8fda9faaf66338a055c
Irina Vaghin <irina.vaghin@webitfactory.io>
2025-04-30 12:10:53 +0300

    Alternative headline logic

--- app/Helpers/JSONLD.php
+++ app/Helpers/JSONLD.php
@@ -3,16 +3,27 @@
 namespace App\Helpers;
 
 use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
 
 class JSONLD
 {
     public static function make($prefix = ''): array
     {
-        return [
-            Textarea::make($prefix . 'jsonld')
-                ->label('JSON-LD')
+        $fields = [
+            Textarea::make('jsonld.description')
+                ->label('JSON-LD description')
                 ->rows(10)
                 ->columnSpan(3),
         ];
+
+        if($prefix == 'blog') {
+            $fields = array_merge($fields, [
+                TextInput::make('jsonld.alternativeHeadline')
+                    ->label('JSON-LD Alternative Headline')
+                    ->columnSpan(3),
+            ]);
+        }
+
+        return $fields;
     }
 }
