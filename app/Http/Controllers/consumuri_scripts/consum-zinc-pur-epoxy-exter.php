fee35a7f5fc908151c551e3a31d11585092005b8
Adrian <adrian.petre@emex.ro>
2025-07-22 11:43:47 +0300

    zinc consum

--- app/Http/Controllers/consumuri_scripts/consum-zinc-pur-epoxy-exter.php
+++ app/Http/Controllers/consumuri_scripts/consum-zinc-pur-epoxy-exter.php
@@ -3,28 +3,25 @@ $tipsuprafata = $_GET[ 'TipSuprafata' ];
 $suprafata = $_GET[ 'Suprafata' ];
 $tipvopsea = $_GET[ 'TipProdus' ];
 if ( $tipsuprafata == "Metal" ) {
-  $consum_vopsea = $suprafata * 0.12;
-  $grund = $suprafata * 0.11;
+  $consum_vopsea = $suprafata * 0.16;
   $diluant = ( $consum_vopsea + $grund ) * 0.1;
   $consum_vopsea = number_format( $consum_vopsea, 2, ",", "." );
-  $grund = number_format( $grund, 2, ",", "." );
   $diluant = number_format( $diluant, 2, ",", "." );
 }
 ?>
 <div class="paint-50 bor-bg">
 <div class="results">
 <div class="cosum text-center">
-<p class="p-space"><strong>Consumul Total:</strong></p>
+<p class="p-space"><strong>Consumul Mediu Informativ:</strong></p>
 <span class="small">la 1 strat</span>
 <p class="num-res"><?php echo $consum_vopsea; ?></p>
 <p><strong>Kg produs</strong></p>
 <p>
 <?php
 if ( $tipsuprafata == "Metal" ) {
-  echo "<strong>Consumul Total</strong> <span class='strat mark'>la 1 strat</span> de <span class='RaspunsEchoBold'>Vopsea Metalizata cu Efect Hammerschlag “Emex”</span> pentru<br />";
+  echo "<strong>Consumul Total</strong> <span class='strat mark'>la 1 strat</span> de <span class='RaspunsEchoBold'>Vopseaua Epoxy-Ester “Emex Zinc Fusion Pro”</span> pentru<br />";
   echo "Suprafata de tip <span class='RaspunsEchoBold'>$tipsuprafata</span> de <span class='RaspunsEchoBold'>$suprafata mp</span>,<br />va fi de aprox. <span class='RaspunsEchoBold'>$consum_vopsea Kg</span> produs.<br />";
   echo "<span id='consumurile_indirecte'>Consum indirect</span>:<br />";
-  echo "Grund Anticoroziv: <span class='RaspunsEchoBold'>$grund Kg</span>.<br />";
   echo "Solvent cca.: <span class='RaspunsEchoBold'>$diluant L</span>.<br />";
 }
 ?>
