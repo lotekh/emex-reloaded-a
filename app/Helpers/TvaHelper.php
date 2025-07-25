6a68cbf98a1f3438cf236d4388e6ef8ca1f69051
DanTudor10 <dan.tudor@webitfactory.io>
2025-07-25 11:52:36 +0300

    feat: add tva helper

---
+++ app/Helpers/TvaHelper.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Helpers;
+
+use Carbon\Carbon;
+
+class TvaHelper
+{
+    public static function getTvaRate(): int
+    {
+        $switchDate = Carbon::create(2025, 8, 1, 0, 0, 0);
+        return now()->lt($switchDate) ? 19 : 21;
+    }
+
+    public static function getTvaMultiplier(): float
+    {
+        return self::getTvaRate() / 100;
+    }
+
+    public static function getPriceWithTvaMultiplier(): float
+    {
+        return 1 + self::getTvaMultiplier();
+    }
+}
