bcdf98818545d8849babda8063f132078f6dacb0
DanTudor10 <dan.tudor@webitfactory.io>
2025-08-07 12:55:11 +0300

    feat: add product_id to DiscountCode

--- app/Models/DiscountCode.php
+++ app/Models/DiscountCode.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class DiscountCode extends Model
 {
@@ -13,11 +14,18 @@ class DiscountCode extends Model
     protected $fillable = [
         'code',
         'percentage',
-        'is_active'
+        'is_active',
+        'product_id',
     ];
 
     public function orders(): HasMany
     {
         return $this->hasMany(Order::class);
     }
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+
 }
