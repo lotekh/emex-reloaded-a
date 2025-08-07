bd67cd5556741a3c5dee7a5775f0fa2976d3d2fb
DanTudor10 <dan.tudor@webitfactory.io>
2025-08-07 18:27:13 +0300

    chore: update ListBlogArticles

--- app/Filament/Resources/BlogArticleResource/Pages/ListBlogArticles.php
+++ app/Filament/Resources/BlogArticleResource/Pages/ListBlogArticles.php
@@ -6,6 +6,7 @@ use App\Filament\Resources\BlogArticleResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class ListBlogArticles extends ListRecords
 {
@@ -22,5 +23,24 @@ class ListBlogArticles extends ListRecords
         return [
             Actions\CreateAction::make(),
         ];
+    }
+
+    public function reorderTable(array $order): void
+    {
+        $orderColumn = 'sort_order';
+
+        DB::transaction(function () use ($order, $orderColumn) {
+            DB::table('blog_articles')
+                ->whereIn('id', array_keys($order))
+                ->update([$orderColumn => null]); 
+
+            foreach ($order as $recordId => $sortOrder) {
+                DB::table('blog_articles')
+                    ->where('id', $recordId)
+                    ->update([
+                        $orderColumn => $sortOrder,
+                    ]);
+            }
+        });
     }
 }
