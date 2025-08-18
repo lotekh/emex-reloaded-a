ead8c158bb6c8af41910fbe9b8783657be30808c
Adrian <adrian.petre@emex.ro>
2025-08-18 14:25:05 +0300

    php polistiren

--- app/Http/Controllers/consumuri_scripts/consum-rasina-decoratiuni-polistiren.php
+++ app/Http/Controllers/consumuri_scripts/consum-rasina-decoratiuni-polistiren.php
@@ -38,11 +38,11 @@ if ( $tipsuprafata == "Suporturi Minerale" ) {
         <?php
         if ( $tipsuprafata == "Polistiren Expandat" ) {
           echo "<strong>Consumul</strong> la 1 strat de <strong class='strat mark'>grosime = 1 mm</strong> de <span class='RaspunsEchoBold'>Pardoseala Epoxidica Antiderapanta “Emex”</span> pentru<br />";
-          echo "Suprafata de tip <span class='RaspunsEchoBold'>$tipsuprafata</span> de <span class='RaspunsEchoBold'>$suprafata mp</span>:<br /> va fi cca. <span class='RaspunsEchoBold'>$consum_vopsea Kg</span> baza epoxidica si <span class='RaspunsEchoBold'>$intaritor Kg</span> intaritor.<br />";
+          echo "Suprafata de tip <span class='RaspunsEchoBold'>$tipsuprafata</span> de <span class='RaspunsEchoBold'>$suprafata mp</span>:<br /> va fi cca. <span class='RaspunsEchoBold'>$consum_vopsea Kg</span> rasina poliuretanica monocomponenta.<br />";
 
           echo "<span id='consumurile_indirecte'>Consum indirect</span>:<br />";
-          echo "Grund Epoxidic de Amorsare: <span class='RaspunsEchoBold'>$grund Kg</span>, si<br />";
-          echo "in medie <span class='RaspunsEchoBold'>$faina_cuart Kg</span> granule de cuartz.<br />";
+         
+          echo "in medie <span class='RaspunsEchoBold'>$faina_cuart Kg</span> faina de cuartz.<br />";
           echo "<span class='strat mark'><strong>Nu se va folosi diluant</strong></span>.<br />";
         }
         if ( $tipsuprafata == "Polistiren Extrudat" ) {
