58948a386e225295918b144ccb870c3a13fb8256
Adrian <adrian.petre@emex.ro>
2025-08-27 09:24:58 +0300

    consum bob orez

--- app/Http/Controllers/consumuri_scripts/consum-lavabila-premium-gold.php
+++ app/Http/Controllers/consumuri_scripts/consum-lavabila-premium-gold.php
@@ -70,9 +70,6 @@ if ( $tipsuprafata == "Glet de Ipsos" ) {
   echo "<strong>Consumul Total</strong> <span class='strat mark'>la 2 straturi</span> de <span class='RaspunsEchoBold'>Vopsea Lavabila Premium de Interior “Emex Gold”</span> pentru<br />";
   echo "Suprafata de tip <span class='RaspunsEchoBold'>$tipsuprafata</span> de <span class='RaspunsEchoBold'>$suprafata mp</span>,<br /> va fi de aprox. <span class='RaspunsEchoBold'>$consum_vopsea Kg</span> produs.<br />";
   echo "<span id='consumurile_indirecte'>Consumuri indirecte</span>:<br />";
-  echo "<strong>Recomandat:</strong><br />";
-  echo "Grund de Amorsare si Umplere Pori: <span class='RaspunsEchoBold'>$grund Kg</span>.<br />";
-  echo "<strong>Obligatoriu:</strong><br />";
   echo "Amorsa (valoare medie): <span class='RaspunsEchoBold'>$amorsa Kg</span>.<br />";
   echo "Apa (valoare medie): <span class='RaspunsEchoBold'>$apa l</span>.<br />";
 }
@@ -80,9 +77,6 @@ if ( $tipsuprafata == "Rigips" ) {
   echo "<strong>Consumul Total</strong> <span class='strat mark'>la 2 straturi</span> de <span class='RaspunsEchoBold'>Vopsea Lavabila Premium de Interior “Emex Gold”</span> pentru<br />";
   echo "Suprafata de tip <span class='RaspunsEchoBold'>$tipsuprafata</span> de <span class='RaspunsEchoBold'>$suprafata mp</span>,<br /> va fi de aprox. <span class='RaspunsEchoBold'>$consum_vopsea Kg</span> produs.<br />";
   echo "<span id='consumurile_indirecte'>Consumuri indirecte</span>:<br />";
-  echo "<strong>Recomandat:</strong><br />";
-  echo "Grund de Amorsare si Umplere Pori: <span class='RaspunsEchoBold'>$grund Kg</span>.<br />";
-  echo "<strong>Obligatoriu:</strong><br />";
   echo "Amorsa (valoare medie): <span class='RaspunsEchoBold'>$amorsa Kg</span>.<br />";
   echo "Apa (valoare medie): <span class='RaspunsEchoBold'>$apa l</span>.<br />";
 }
