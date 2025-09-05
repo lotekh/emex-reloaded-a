5a41db642582ffa5d1fa8bd02c33bb27ead6236e
Adrian <adrian.petre@emex.ro>
2025-09-05 13:55:24 +0300

    par metal

--- app/Http/Controllers/consumuri_scripts/consum-pardoseala-epoxidica-metalica.php
+++ app/Http/Controllers/consumuri_scripts/consum-pardoseala-epoxidica-metalica.php
@@ -8,10 +8,12 @@ if ( $tipsuprafata == "Sapa" ) {
   $cons_total = $consum_vopsea + $intaritor;
   $grund = $suprafata * 0.14;
   $fond = $suprafata * 0.25;
+  $pigment = $cons_total * 0.05;
   $consum_vopsea = number_format( $consum_vopsea, 2, ",", "." );
   $intaritor = number_format( $intaritor, 2, ",", "." );
   $grund = number_format( $grund, 2, ",", "." );
   $fond = number_format( $fond, 2, ",", "." );
+  $pigment = number_format( $fond, 2, ",", "." );
   $cons_total = number_format( $cons_total, 2, ",", "." );
 }
 if ( $tipsuprafata == "Sapa Sclivisita" ) {
@@ -20,10 +22,12 @@ if ( $tipsuprafata == "Sapa Sclivisita" ) {
   $cons_total = $consum_vopsea + $intaritor;
   $grund = $suprafata * 0.14;
   $fond = $suprafata * 0.25;
+  $pigment = $cons_total * 0.05;
   $consum_vopsea = number_format( $consum_vopsea, 2, ",", "." );
   $intaritor = number_format( $intaritor, 2, ",", "." );
   $grund = number_format( $grund, 2, ",", "." );
   $fond = number_format( $fond, 2, ",", "." );
+  $pigment = number_format( $fond, 2, ",", "." );
   $cons_total = number_format( $cons_total, 2, ",", "." );
 }
 if ( $tipsuprafata == "Beton Elicopterizat" ) {
@@ -32,10 +36,12 @@ if ( $tipsuprafata == "Beton Elicopterizat" ) {
   $cons_total = $consum_vopsea + $intaritor;
   $grund = $suprafata * 0.12;
   $fond = $suprafata * 0.25;
+  $pigment = $cons_total * 0.05;
   $consum_vopsea = number_format( $consum_vopsea, 2, ",", "." );
   $intaritor = number_format( $intaritor, 2, ",", "." );
   $grund = number_format( $grund, 2, ",", "." );
   $fond = number_format( $fond, 2, ",", "." );
+  $pigment = number_format( $fond, 2, ",", "." );
   $cons_total = number_format( $cons_total, 2, ",", "." );
 }
 if ( $tipsuprafata == "Mozaic" ) {
@@ -44,10 +50,12 @@ if ( $tipsuprafata == "Mozaic" ) {
   $cons_total = $consum_vopsea + $intaritor;
   $grund = $suprafata * 0.11;
   $fond = $suprafata * 0.25;
+  $pigment = $cons_total * 0.05;
   $consum_vopsea = number_format( $consum_vopsea, 2, ",", "." );
   $intaritor = number_format( $intaritor, 2, ",", "." );
   $grund = number_format( $grund, 2, ",", "." );
   $fond = number_format( $fond, 2, ",", "." );
+  $pigment = number_format( $fond, 2, ",", "." );
   $cons_total = number_format( $cons_total, 2, ",", "." );
 }
 ?>
@@ -65,10 +73,8 @@ if ( $tipsuprafata == "Mozaic" ) {
           echo "Suprafata de tip <span class='RaspunsEchoBold'>$tipsuprafata</span> de <span class='RaspunsEchoBold'>$suprafata mp</span>,<br /> va fi de aprox. <span class='RaspunsEchoBold'>$consum_vopsea Kg</span> baza si <span class='RaspunsEchoBold'>$intaritor Kg</span> intaritor.<br />";
           echo "<span id='consumurile_indirecte'>Consum indirect</span>:<br />";
           echo "Grund Epoxidic de Amorsare: <span class='RaspunsEchoBold'>$grund Kg</span><br />";
-
           echo "Vopsea Epoxidica de Pardoseala „Emex”: <span class='RaspunsEchoBold'>$fond Kg</span><br />";
-
-
+          echo "Pigment metalic: cca. <span class='RaspunsEchoBold'>$pigment Kg</span><br />";
           echo "<span class='strat mark'><strong>Nu se va folosi diluant</strong></span>.<br />";
         }
         if ( $tipsuprafata == "Sapa Sclivisita" ) {
@@ -77,6 +83,7 @@ if ( $tipsuprafata == "Mozaic" ) {
           echo "<span id='consumurile_indirecte'>Consum indirect</span>:<br />";
           echo "Grund Epoxidic de Amorsare: <span class='RaspunsEchoBold'>$grund Kg</span><br />";
           echo "Vopsea Epoxidica de Pardoseala „Emex”: <span class='RaspunsEchoBold'>$fond Kg</span><br />";
+          echo "Pigment metalic: cca. <span class='RaspunsEchoBold'>$pigment Kg</span><br />";
           echo "<span class='strat mark'><strong>Nu se va folosi diluant</strong></span>.<br />";
         }
         if ( $tipsuprafata == "Beton Elicopterizat" ) {
@@ -85,6 +92,7 @@ if ( $tipsuprafata == "Mozaic" ) {
           echo "<span id='consumurile_indirecte'>Consum indirect</span>:<br />";
           echo "Grund Epoxidic de Amorsare: <span class='RaspunsEchoBold'>$grund Kg</span><br />";
           echo "Vopsea Epoxidica de Pardoseala „Emex”: <span class='RaspunsEchoBold'>$fond Kg</span><br />";
+          echo "Pigment metalic: cca. <span class='RaspunsEchoBold'>$pigment Kg</span><br />";
           echo "<span class='strat mark'><strong>Nu se va folosi diluant</strong></span>.<br />";
         }
         if ( $tipsuprafata == "Mozaic" ) {
@@ -93,6 +101,7 @@ if ( $tipsuprafata == "Mozaic" ) {
           echo "<span id='consumurile_indirecte'>Consum indirect</span>:<br />";
           echo "Grund Epoxidic de Amorsare: <span class='RaspunsEchoBold'>$grund Kg</span><br />";
           echo "Vopsea Epoxidica de Pardoseala „Emex”: <span class='RaspunsEchoBold'>$fond Kg</span><br />";
+          echo "Pigment metalic: cca. <span class='RaspunsEchoBold'>$pigment Kg</span><br />";
           echo "<span class='strat mark'><strong>Nu se va folosi diluant</strong></span>.<br />";
         }
         ?>
